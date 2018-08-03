3596d5b5dc6485c232e6ccbf3edff36a4aa631fb
sherlock kong <sherlock.kong@grapecity.com>
2018-08-03 23:59:03 +0800

    add favicon and compression..

--- sample/server.js
+++ sample/server.js
@@ -1,8 +1,10 @@
 var express = require('express')
+var compression = require('compression')
 var app = express()
 var path = require('path')
 
 app.set('port', 9898)
+app.use(compression())
 app.use(express.static(path.join(__dirname, '/public')))
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '/public/index.html'))
