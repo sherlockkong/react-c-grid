0c6ff5bcb048c1e255583f3487ca6b33147522cf
sherlock kong <sherlock.kong@grapecity.com>
2019-03-03 00:51:58 +0800

    use production NODE_ENV to start production sample

--- sample/server.js
+++ sample/server.js
@@ -7,7 +7,7 @@ app.set('port', 9898);
 app.use(compression());
 app.use(express.static(path.join(__dirname, '/public')));
 app.get(/(?<!\.json)$/, function (req, res) {
-	res.sendFile(path.join(__dirname, process.env.NODE_ENV === 'development' ? '/debug.html' : '/index.html'));
+	res.sendFile(path.join(__dirname, process.env.NODE_ENV === 'production' ? '/index.html' : '/debug.html'));
 });
 app.listen(app.get('port'), function () {
 	console.log("Node app is running at localhost:" + app.get('port'))
