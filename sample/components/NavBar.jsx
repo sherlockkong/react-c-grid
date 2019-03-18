a319ee9dce7cb1361572a3a07b27011849cb2de8
sherlock kong <sherlock.kong@grapecity.com>
2019-03-18 23:33:47 +0800

    add github link for demo navbar

--- sample/components/NavBar.jsx
+++ sample/components/NavBar.jsx
@@ -10,6 +10,7 @@ class NavBar extends React.PureComponent {
 
 		return (
 			<div className='nav-bar'>
+				<a className="nav-item mdi mdi-github-circle" href="https://github.com/sherlockkong/react-c-grid" />
 				<button className={`nav-item mdi ${mode === 'GRID' ? 'mdi-code-tags' : 'mdi-grid-large'}`}
 					onClick={() => dispatch(AppActions.Demo.SetDemoMode(nextMode))}
 				/>
